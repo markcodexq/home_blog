acb7004c1b1ef66b6b2f0af182fddd44cfdfc676
markcodexq <markcodexq@gmail.com>
2022-09-24 18:02:08 +0500

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  before_action :set_params, only: %i(create destroy)
+
+  def create
+    @comment = @post.comments.create(params[:comment].permit(:title, :comment))
+    redirect_to edit_post_path(@post)
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def set_params
+    @post = Post.find(params[:post_id])
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(post_params)
+
+    redirect_to posts_path
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
